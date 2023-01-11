033154e5757ceac8f64d266a5c3e0e46e83ab328
dieguitocano <dieguito.kano@gmail.com>
2023-01-11 00:52:13 -0500

    last version

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import './style/aboutMe.css'
 
+
 const AboutMe = () => {
+
+
   return (
     <div className='general-aboutme-container'>
       <div className='about-me-container'>
@@ -11,17 +14,12 @@ const AboutMe = () => {
       </div>
       <div className='about-card-container'>
         <div className='about-me-card'>
-          <p>Full Stack Development student, just finished Front end module
-            as an outstanding student.
-            Individual with a high sense of aesthethics.
-            Software Development is the right tool for me to transform through shapes and effects, transfrom through beauty, it is my way of sharing peace and harmony
+          <p>Front end React Developer with a high sense of aesthethics and always open to learn new technologies.
+            Software Development is the right tool for me to transform through shapes and effects, transfrom through beauty, it is my way of sharing |peace and harmony
           </p>
         </div>
       </div>
     </div>
-
-
-
   )
 }
 
